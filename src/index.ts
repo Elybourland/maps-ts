22c16862c91cf71b36b1b6b24da2f1bf96d91f68
Elybourland <JEBourland@yahoo.com>
2022-07-15 14:25:21 -0700

    defining a company

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { User } from "./User";
+import { Company } from "./Company";
 
 // parcel index.html in cmd line to run in browser
 
 const user = new User();
 console.log(user);
+
+const company = new Company();
+console.log(company);
