03df421665133d8d4873cb342af752b01f8684cb
Elybourland <JEBourland@yahoo.com>
2022-07-19 10:24:20 -0700

    optional implements clauses

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -136888,6 +136888,7 @@ var User =
 /** @class */
 function () {
   function User() {
+    this.color = 'red';
     this.name = faker_1.default.name.firstName();
     this.location = {
       lat: parseFloat(faker_1.default.address.latitude()),
@@ -136923,6 +136924,7 @@ var Company =
 /** @class */
 function () {
   function Company() {
+    this.color = 'red';
     this.companyName = faker_1.default.company.companyName();
     this.catchPhrase = faker_1.default.company.catchPhrase();
     this.location = {
@@ -137038,7 +137040,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62250" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65433" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
